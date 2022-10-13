b386d6f6dbfdc9432cdf32e2ae3a001b68b64282
sclecon <27941662@qq.com>
2022-10-13 16:52:50 +0800

    修复管理方法错误问题

--- src/app/Controller/Admin/AuctionImages.php
+++ src/app/Controller/Admin/AuctionImages.php
@@ -5,7 +5,7 @@ namespace App\Controller\Admin;
 use App\Annotation\ApiRouter;
 use App\Annotation\Validator;
 use App\Controller\Admin\Base\BaseCurd;
-use App\Model\BannerModel;
+use App\Model\AuctionImageModel;
 use App\Utils\Image;
 
 /**
@@ -15,7 +15,7 @@ class AuctionImages extends BaseCurd
 {
     public function __construct()
     {
-        $this->model = new BannerModel();
+        $this->model = new AuctionImageModel();
         parent::__construct();
     }
 
--- src/app/Controller/Admin/AuctionTopic.php
+++ src/app/Controller/Admin/AuctionTopic.php
@@ -3,10 +3,8 @@
 namespace App\Controller\Admin;
 
 use App\Annotation\ApiRouter;
-use App\Annotation\Validator;
 use App\Controller\Admin\Base\BaseCurd;
-use App\Model\BannerModel;
-use App\Utils\Image;
+use App\Model\AuctionTopicModel;
 
 /**
  * @ApiRouter(router="admin/auction/topic", method="get", intro="拍品拍场")
@@ -15,7 +13,7 @@ class AuctionTopic extends BaseCurd
 {
     public function __construct()
     {
-        $this->model = new BannerModel();
+        $this->model = new AuctionTopicModel();
         parent::__construct();
     }
 }
