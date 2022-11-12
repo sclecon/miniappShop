f49d389395c60ce19c5cd25d3a7e3653d07f52c7
sclecon <27941662@qq.com>
2022-11-12 16:49:51 +0800

    upgrade

--- src/app/Utils/WeChat.php
+++ src/app/Utils/WeChat.php
@@ -21,20 +21,16 @@ class WeChat
 
     public static function app() {
         if (is_null(self::$app)){
-            $container = ApplicationContext::getContainer();
 
             $app = Factory::officialAccount(ConfigService::instance()->getWechatConfig());
             $handler = new CoroutineHandler();
 
-// 设置 HttpClient，部分接口直接使用了 http_client。
             $config = $app['config']->get('http', []);
             $config['handler'] = $stack = HandlerStack::create($handler);
             $app->rebind('http_client', new Client($config));
 
-// 部分接口在请求数据时，会根据 guzzle_handler 重置 Handler
             $app['guzzle_handler'] = $handler;
 
-// 如果使用的是 OfficialAccount，则还需要设置以下参数
             $app->oauth->setGuzzleOptions([
                 'http_errors' => false,
                 'handler' => $stack,
@@ -49,10 +45,12 @@ class WeChat
             $server = $hyperfRequest->getServerParams();
             $xml = $hyperfRequest->getBody()->getContents();
             $files = [];
+
             /** @var \Hyperf\HttpMessage\Upload\UploadedFile $v */
             foreach ($uploadFiles as $k => $v) {
                 $files[$k] = $v->toArray();
             }
+
             $request = new Request($get, $post, [], $cookie, $files, $server, $xml);
             $request->headers = new HeaderBag($hyperfRequest->getHeaders());
             $app->rebind('request', $request);
