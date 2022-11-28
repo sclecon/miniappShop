fb1f4e20bb1f3991d1224264a7ae6efa992b4226
sclecon <27941662@qq.com>
2022-11-28 10:03:50 +0800

    upgrade

--- src/app/Controller/Port/Painter.php
+++ src/app/Controller/Port/Painter.php
@@ -13,10 +13,10 @@ use App\Services\PainterService;
 class Painter extends BaseSupportController
 {
     /**
-     * @ApiRouter(router="get", method="get", intro="获取画家详情")
+     * @ApiRouter(router="detail", method="get", intro="获取画家详情")
      * @Validator(attribute="painter_id", required=true, rule="integer", intro="画家ID")
      */
-    public function delete(){
+    public function detail(){
         $painterId = (int) $this->request->input('painter_id', 0);
         $painter = PainterService::instance()->detail($painterId);
         return $painter ? $this->success('获取画家详情成功', $painter) : $this->error('画家不存在');
